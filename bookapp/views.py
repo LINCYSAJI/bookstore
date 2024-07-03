0e05a4b1daf559c37b45d73678b5258e4c5fcdd2
LINCYSAJI <104355791+LINCYSAJI@users.noreply.github.com>
2024-07-03 08:36:05 +0530

    register page start

--- bookapp/views.py
+++ bookapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from bookapp.forms import SignUpForm
 
@@ -11,4 +11,19 @@ class RegistrationView(View):
         form_instance=SignUpForm()
         
         return render(request,"register.html",{"form":form_instance})
+    
+    def post(self,request,*args, **kwargs):
+        
+        form_instance=SignUpForm(request.POST)
+        
+        if form_instance.is_valid():
+            
+            form_instance.save()
+            
+            print("Account created")
+            
+            return redirect("register")
+        
+        return render(request,"register.html",{"form":form_instance})
+
 
